109eec9bfee5b0017178a18d36d68b5eba6b5b2f
Sautron Gaël <gael.sautron@pacificproweb.nc>
2023-03-26 18:07:01 +1100

    feat: add dark theme

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function Greeting({messages}:{messages:string[]}) {
+
+    const randomMessage = () => messages[(Math.floor(Math.random() * messages.length))];
+    
+    const [greeting, setGreeting] = useState(messages[0]);
+  
+    return (
+      <div> 
+        <h3>{greeting}! Thank you for visiting!</h3>
+        <button onClick={(() => {
+            setGreeting(randomMessage());
+            console.log("here");
+        })}>
+          New Greeting
+        </button>
+      </div>
+    );
+  }
